58bc836cff0f4b6266df983c0de6aa4e465c31e5
odedmar <odedd.markovich@gmail.com>
2014-07-25 20:59:49 +0300

    some changes

--- Anangular-TUTExample/WebContent/js/controllers.js
+++ Anangular-TUTExample/WebContent/js/controllers.js
@@ -1,4 +1,36 @@
 /**
  * Module wich will be hold the app controllers.
  */
-nn
+
+
+(function(){
+	
+	
+	var appContrl = angular.module('appContrl',[]);
+	
+	appContrl.controller('pohoneList',['$http',function($http){
+		
+	
+	var currentScope = this;	
+	this.orderProp='name';	
+	
+		
+	$http.get('phones/phones.json').success(function(data){
+		
+		currentScope.phones = data;
+	});
+	
+			
+	}]);	
+	
+appContrl.controller('phoneDetailCtl',['$routeParams',function($routeParams){
+		
+	
+		
+		this.phoneId = $routeParams.phoneId;
+		
+		
+	}]);
+	
+	
+})();
