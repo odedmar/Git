9b0b2c072f1d2f5eed18695aab15c9429374ef74
odedmar <odedd.markovich@gmail.com>
2014-08-05 19:15:14 +0300

    final sample

--- Anangular-TUTExample/WebContent/js/app.js
+++ Anangular-TUTExample/WebContent/js/app.js
@@ -1,39 +1,28 @@
-/** 
- * New node file
- */
+'use strict';
 
+/* App Module */
 
-(function(){
-	
-	
-	var appPhone = angular.module('appPone',['appContrl','ngRoute','ng']);
-	appPhone.config(['$routeProvider',function($routeProvider){
-		
-		//alert('url = ' + $location.absUrl());
-		
-		$routeProvider.when('/phones',
-							{  
-								templateUrl: 'partials/phone-list.html',
-								controller : 'pohoneList as phoneList'
-							}
-		
-		
-		).when('/phones/:phoneId',
-							{
-								templateUrl: 'partials/phone-detail.html',
-								controller: 'phoneDetailCtl as pctl'	
-							}
-				
-		).otherwise({
-						redirectTo: '/phones'
-					}
-		);
-		
-		
-	}]);
-	
-})();
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
 
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
 
-
-
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
--- Anangular-TUTExample/WebContent/js/controllers.js
+++ Anangular-TUTExample/WebContent/js/controllers.js
@@ -1,36 +1,22 @@
-/**
- * Module wich will be hold the app controllers.
- */
+'use strict';
 
+/* Controllers */
 
-(function(){
-	
-	
-	var appContrl = angular.module('appContrl',[]);
-	
-	appContrl.controller('pohoneList',['$http',function($http){
-		
-	
-	var currentScope = this;	
-	this.orderProp='name';	
-	
-		
-	$http.get('phones/phones.json').success(function(data){
-		
-		currentScope.phones = data;
-	});
-	
-			
-	}]);	
-	
-appContrl.controller('phoneDetailCtl',['$routeParams',function($routeParams){
-		
-	
-		
-		this.phoneId = $routeParams.phoneId;
-		
-		
-	}]);
-	
-	
-})();
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
